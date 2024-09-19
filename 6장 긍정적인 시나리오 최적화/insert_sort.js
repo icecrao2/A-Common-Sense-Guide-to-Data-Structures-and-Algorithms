f0e734775771ecfcafba307d1de1ee6e0592d937
icecrao2 <icecrao2@daum.net>
2024-09-19 22:40:56 +0900

    6장 긍정적인 시나리오 최적화

---
+++ 6장 긍정적인 시나리오 최적화/insert_sort.js
@@ -0,0 +1,24 @@
+
+
+function insertSort(array) {
+
+    for(var standardIdx = 1; standardIdx < array.length; standardIdx++) {
+
+        var tempData = array[standardIdx];
+
+        for(var idx = (standardIdx - 1); idx >= 0; idx--) {
+
+            if(tempData < array[idx]) {
+                array[idx + 1] = array[idx];
+            } else {
+                array[idx + 1] = tempData;
+                break;
+            }
+        }
+    }
+
+    return array;
+}
+
+var result = insertSort([2,5,3,5,7,2,24,5,4,6,4,3,6,5,7,5,53,]);
+console.log(result);
