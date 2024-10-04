47e601d780cd3eede2e78053bf78af4a4eda8392
icecrao2 <icecrao2@daum.net>
2024-10-04 17:38:46 +0900

    13장 속도를 높이는 재귀 알고리즘

--- 13장 속도를 높이는 재귀 알고리즘/find_max_number.js
+++ 13장 속도를 높이는 재귀 알고리즘/find_max_number.js
@@ -14,3 +14,11 @@ function findMaxNumberByN2(array, max = 0) {
 
 var result1 = findMaxNumberByN2([1,3,5,2,3,4]);
 console.log(result1);
+
+function findMaxNumberByNLogN(array) {
+
+}
+
+function findMaxNumberByN2(array) {
+
+}
